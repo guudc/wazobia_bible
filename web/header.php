70f46f047431f759d3021380325dc7dabf673b17
guudc <65718626+guudc@users.noreply.github.com>
2024-01-09 21:03:26 +0100

    Add files via upload

---
+++ web/header.php
@@ -0,0 +1,93 @@
+<!-- Navigation Bar-->
+<header id="topnav">
+    <nav class="navbar-custom">
+        <ul class="list-unstyled topbar-right-menu float-right mb-0">
+
+            <li class="dropdown notification-list">
+                <a class="nav-link dropdown-toggle nav-user" data-toggle="dropdown" href="#" role="button"
+                   aria-haspopup="false" aria-expanded="false">
+                    <img src="assets/images/users/avatar-1.jpg" alt="user" class="rounded-circle"> <span class="ml-1">Administrator <i class="mdi mdi-chevron-down"></i> </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated profile-dropdown ">
+                    <!-- item-->
+                    <div class="dropdown-item noti-title">
+                        <h6 class="text-overflow m-0">Welcome !</h6>
+                    </div>
+
+
+                    <!-- item-->
+                    <a href="logout.php" class="dropdown-item notify-item">
+                        <i class="dripicons-power"></i> <span>Logout</span>
+                    </a>
+
+                </div>
+            </li>
+
+        </ul>
+
+        <ul class="list-unstyled menu-left mb-0">
+            <li class="float-left">
+                <a href="index" class="logo">
+                                <span class="logo-lg">
+                                    Sharepoint
+                                </span>
+                    <span class="logo-sm">
+                    Sharepoint
+                                </span>
+                </a>
+            </li>
+            <li class="float-left">
+                <a class="button-menu-mobile open-left navbar-toggle">
+                    <div class="lines">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </a>
+            </li>
+            <li class="app-search">
+                <form>
+                    <input type="text" placeholder="Search..." class="form-control">
+                    <button type="submit" class="sr-only"></button>
+                </form>
+            </li>
+        </ul>
+    </nav>
+    <!-- end navbar-custom -->
+</header>
+<!-- End Navigation Bar-->
+
+<!-- ========== Left Sidebar Start ========== -->
+<div class="left-side-menu">
+
+    <div class="slimscroll-menu">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+
+            <ul class="metismenu" id="side-menu">
+
+                <li class="menu-title">Navigation</li>
+
+                <li>
+                   <a href="index.html">Users Logins</a>
+                </li>
+                <li><a href="editadmin.html">Admins </a></li>
+                <li><a href="logout.html">Log out </a></li>
+                
+
+
+
+            </ul>
+
+        </div>
+        <!-- Sidebar -->
+
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
+<!-- Left Sidebar End -->
+ 
